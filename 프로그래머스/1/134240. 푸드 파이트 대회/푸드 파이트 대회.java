935e85a930e976f67f8e8b211aa0a2c5080cb90c
subin <143770193+LSLight@users.noreply.github.com>
2026-03-16 10:59:53 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.08 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<food.length; i++){
+            for(int j=0; j<food[i]/2; j++){
+                sb.append(i);
+            }
+        }
+        String left = sb.toString();
+        
+        return left+"0"+sb.reverse().toString();
+    }
+}
