ab41319e7d8f527b2ca1b0b98680c6609b44f2bb
subin <143770193+LSLight@users.noreply.github.com>
2026-03-23 10:38:06 +0900

    [level 1] Title: 과일 장수, Time: 89.24 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int sum = 0;
+        Arrays.sort(score);
+        
+        for(int i = score.length-m; i>=0; i-=m){
+            sum += score[i] * m;
+        }
+        return sum;
+
+    }
+}
